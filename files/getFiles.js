d9d27628f687aab0cfc12528666ba2d84193e2a8
da-v-id <da.v.id@live.com>
2015-10-24 10:36:03 +0100

    Update getFiles.js

--- files/getFiles.js
+++ files/getFiles.js
@@ -1,1 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+var pdfText = require('pdf-text');
+var cheerio = require("cheerio");
+var request = require("request");
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+var path = require('path');
 
+var urlsArray = [];
+
+request({
+  uri: "http://hymns.countedfaithful.org/numberListing.php",
+}, function(error, response, body) {
+  var $ = cheerio.load(body);
+  $('a').each(function(){
+    var link = $(this);
+    var href = link.attr('href');
+    if(href.indexOf('.pdf')!==(-1)){
+        urlsArray.push(href);
+    }
+  });
+  buildDB();
+});
