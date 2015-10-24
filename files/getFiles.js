a8d83f81d07dedffe50400ded0075666b35ff6b8
da-v-id <da.v.id@live.com>
2015-10-24 10:52:01 +0100

    Update getFiles.js

--- files/getFiles.js
+++ files/getFiles.js
@@ -24,40 +24,11 @@ request({
 
 function processPdf(uri){
 
-var file = fs.createWriteStream(path.basename(uri));
-var hymnObj = {};
-var request = http.get(uri, function(response) {
+  var file = fs.createWriteStream(path.basename(uri));
+  var hymnObj = {};
+  var request = http.get(uri, function(response) {
     var r = response.pipe(file);
-    r.on('finish', function () {
-        var pathToPdf = __dirname + "/"+path.basename(uri);
-
-        
-
-        pdfText(pathToPdf, function(err, chunks) {
-            chunks = getNumber(chunks);
-            chunks = getVersion(chunks);
-            chunks = getTitle(chunks);
-            chunks = getBeats(chunks);
-            chunks = getSubTitle(chunks);
-            chunks = resetFirstLetter(chunks);
-            chunks = getCopyright(chunks);
-            chunks = getAuthor(chunks);
-            chunks = seperateToVerses(chunks);
-            hymnObj.tune = "";
-
-            collection.insert(hymnObj, {w: 1}, function(err, result){
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Inserted '+hymnObj.number);
-              }
-              //Close connection
-              
-            });
-          //chunks is an array of strings 
-          //loosely corresponding to text objects within the pdf
-
-          //for a more concrete example, view the test file in this repo
-        });
-    });
+    
+  });
+  fs.stream.end();
 });
