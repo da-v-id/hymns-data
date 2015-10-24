ac266908d22188944bdf6b06363acea8b41640d8
da-v-id <da.v.id@live.com>
2015-10-24 10:43:45 +0100

    Update getFiles.js

--- files/getFiles.js
+++ files/getFiles.js
@@ -7,7 +7,6 @@ var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
 var path = require('path');
 
-var urlsArray = [];
 
 request({
   uri: "http://hymns.countedfaithful.org/numberListing.php",
@@ -17,8 +16,48 @@ request({
     var link = $(this);
     var href = link.attr('href');
     if(href.indexOf('.pdf')!==(-1)){
-        urlsArray.push(href);
+      processPdf(href);
     }
   });
   buildDB();
+});
+
+function processPdf(uri){
+
+var file = fs.createWriteStream(path.basename(uri));
+var hymnObj = {};
+var request = http.get(uri, function(response) {
+    var r = response.pipe(file);
+    r.on('finish', function () {
+        var pathToPdf = __dirname + "/"+path.basename(uri);
+
+        
+
+        pdfText(pathToPdf, function(err, chunks) {
+            chunks = getNumber(chunks);
+            chunks = getVersion(chunks);
+            chunks = getTitle(chunks);
+            chunks = getBeats(chunks);
+            chunks = getSubTitle(chunks);
+            chunks = resetFirstLetter(chunks);
+            chunks = getCopyright(chunks);
+            chunks = getAuthor(chunks);
+            chunks = seperateToVerses(chunks);
+            hymnObj.tune = "";
+
+            collection.insert(hymnObj, {w: 1}, function(err, result){
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Inserted '+hymnObj.number);
+              }
+              //Close connection
+              
+            });
+          //chunks is an array of strings 
+          //loosely corresponding to text objects within the pdf
+
+          //for a more concrete example, view the test file in this repo
+        });
+    });
 });
