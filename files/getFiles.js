add707aac05f5a4c2b2fdb8a0595b5287269339f
da-v-id <da.v.id@live.com>
2015-11-19 23:17:55 +0000

    Update getFiles.js

--- files/getFiles.js
+++ files/getFiles.js
@@ -3,10 +3,8 @@ var fs = require('fs');
 var cheerio = require("cheerio");
 var request = require("request");
 var path = require('path');
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
 
-var run = async (function (){
+var run = unction (){
   request({
     uri: "http://hymns.countedfaithful.org/numberListing.php",
   }, function(error, response, body) {
@@ -15,18 +13,18 @@ var run = async (function (){
       var link = $(this);
       var href = link.attr('href');
       if(href.indexOf('.pdf')!==(-1)){
-        var result = await (processPdf(href));
+        var result = processPdf(href);
       }
     });
   });
-});
+}
 
-var processPdf = async (function (uri){
+var processPdf = function (uri){
 
   var file = fs.createWriteStream('pdfs/'+path.basename(uri));
   var hymnObj = {};
-  var request = await (http.get(uri, function(response) {
+  var request = http.get(uri, function(response) {
     var r = response.pipe(file);
     
-  }));
-});
+  });
+}
