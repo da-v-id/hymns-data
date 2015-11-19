7efaf387cffe0097ad9c2c96a455d27773586a17
da-v-id <da.v.id@live.com>
2015-11-19 23:11:36 +0000

    Update getFiles.js

--- files/getFiles.js
+++ files/getFiles.js
@@ -3,27 +3,37 @@ var fs = require('fs');
 var cheerio = require("cheerio");
 var request = require("request");
 var path = require('path');
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 
-
-request({
-  uri: "http://hymns.countedfaithful.org/numberListing.php",
-}, function(error, response, body) {
-  var $ = cheerio.load(body);
-  $('a').each(function(){
-    var link = $(this);
-    var href = link.attr('href');
-    if(href.indexOf('.pdf')!==(-1)){
-      processPdf(href);
-    }
+var run = async function (){
+  request({
+    uri: "http://hymns.countedfaithful.org/numberListing.php",
+  }, function(error, response, body) {
+    var $ = cheerio.load(body);
+    $('a').each(function(){
+      var link = $(this);
+      var href = link.attr('href');
+      if(href.indexOf('.pdf')!==(-1)){
+        var result = await processPdf(href);
+      }
+    });
   });
-});
+}
 
-function processPdf(uri){
+var processPdf = async (function (uri){
 
   var file = fs.createWriteStream('pdfs/'+path.basename(uri));
   var hymnObj = {};
-  var request = http.get(uri, function(response) {
+  var request = await http.get(uri, function(response) {
     var r = response.pipe(file);
     
   });
-}
+});
+
+var go = async (function() {
+    var resultA = await (firstAsyncCall());
+    var resultB = await (secondAsyncCallUsing(resultA));
+    var resultC = await (thirdAsyncCallUsing(resultB));
+    return doSomethingWith(resultC);
+});
