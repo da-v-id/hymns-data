3ac4add895d904b9eb167c433863f276ad2c5916
da-v-id <da.v.id@live.com>
2015-11-19 23:25:37 +0000

    Update getFiles.js

--- files/getFiles.js
+++ files/getFiles.js
@@ -4,7 +4,9 @@ var cheerio = require("cheerio");
 var request = require("request");
 var path = require('path');
 
-var run = unction (){
+var linkArray = []
+
+var run = function (){
   request({
     uri: "http://hymns.countedfaithful.org/numberListing.php",
   }, function(error, response, body) {
@@ -13,9 +15,11 @@ var run = unction (){
       var link = $(this);
       var href = link.attr('href');
       if(href.indexOf('.pdf')!==(-1)){
+        linkArray[] = href;
         var result = processPdf(href);
       }
     });
+    processor(0);
   });
 }
 
@@ -23,8 +27,15 @@ var processPdf = function (uri){
 
   var file = fs.createWriteStream('pdfs/'+path.basename(uri));
   var hymnObj = {};
-  var request = http.get(uri, function(response) {
+  var request = http.get(linkArray[uri], function(response) {
     var r = response.pipe(file);
-    
+    processor(uri+=1);
   });
+}
+
+var processor = function(num){
+  if(linkArray[num]){
+    processPdf(num);
+  }
+  
 }
