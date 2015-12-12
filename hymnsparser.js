dcee16971c9c553861e268d73e96d26bf15fb575
da-v-id <da.v.id@live.com>
2015-12-12 15:34:08 +0000

    Update hymnsparser.js

--- hymnsparser.js
+++ hymnsparser.js
@@ -1,29 +1,8 @@
-var http = require('http');
-var fs = require('fs');
 var pdfText = require('pdf-text');
-var cheerio = require("cheerio");
-var request = require("request");
 var MongoClient = require('mongodb').MongoClient;
-var format = require('util').format;
 var path = require('path');
 
 var urlsArray = [];
-
-/*request({
-  uri: "http://hymns.countedfaithful.org/numberListing.php",
-}, function(error, response, body) {
-  var $ = cheerio.load(body);
-  $('a').each(function(){
-    var link = $(this);
-    var href = link.attr('href');
-    if(href.indexOf('.pdf')!==(-1)){
-        urlsArray.push(href);
-    }
-  });
-  buildDB();
-});*/
-
-
 
 function buildDB(){
     MongoClient.connect('mongodb://127.0.0.1:27017/hymns', function(err, db) {
@@ -34,46 +13,31 @@ function buildDB(){
         urlsArray.forEach(function(element, index, array){
             processPdf(element);
         });
-
-//processPdf(urlsArray[urlsArray.length-1]);
         
         function processPdf(uri){
 
-            var file = fs.createWriteStream(path.basename(uri));
             var hymnObj = {};
-            var request = http.get(uri, function(response) {
-                var r = response.pipe(file);
-                r.on('finish', function () {
-                    var pathToPdf = __dirname + "/"+path.basename(uri);
+            var pathToPdf = __dirname + "/"+path.basename(uri);
 
-                    
+            pdfText(pathToPdf, function(err, chunks) {
+                chunks = getNumber(chunks);
+                chunks = getVersion(chunks);
+                chunks = getTitle(chunks);
+                chunks = getBeats(chunks);
+                chunks = getSubTitle(chunks);
+                chunks = resetFirstLetter(chunks);
+                chunks = getCopyright(chunks);
+                chunks = getAuthor(chunks);
+                chunks = seperateToVerses(chunks);
+                hymnObj.tune = "";
 
-                    pdfText(pathToPdf, function(err, chunks) {
-                        chunks = getNumber(chunks);
-                        chunks = getVersion(chunks);
-                        chunks = getTitle(chunks);
-                        chunks = getBeats(chunks);
-                        chunks = getSubTitle(chunks);
-                        chunks = resetFirstLetter(chunks);
-                        chunks = getCopyright(chunks);
-                        chunks = getAuthor(chunks);
-                        chunks = seperateToVerses(chunks);
-                        hymnObj.tune = "";
-
-                        collection.insert(hymnObj, {w: 1}, function(err, result){
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                console.log('Inserted '+hymnObj.number);
-                          }
-                          //Close connection
-                          
-                        });
-                      //chunks is an array of strings 
-                      //loosely corresponding to text objects within the pdf
-
-                      //for a more concrete example, view the test file in this repo
-                    });
+                collection.insert(hymnObj, {w: 1}, function(err, result){
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Inserted '+hymnObj.number);
+                    }
+                  //Close connection
                 });
             });
 
